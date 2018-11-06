bad43cc046682d4a36ac2f51b2f85e533df9914a
Meng <lingxmeng@ebay.com>
2018-11-06 15:57:59 +0800

    Facebook login

--- js/waterfall.js
+++ js/waterfall.js
@@ -13,6 +13,13 @@
      * 内容JSON
      */
   var demoContent = [
+      {
+          demo_link: 'https://blog.csdn.net/Jeffrey20170812/article/details/83575754',
+          img_link: 'https://img-blog.csdnimg.cn/20181031101022866.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L0plZmZyZXkyMDE3MDgxMg==,size_16,color_FFFFFF,t_70',
+          title: '第三方登陆实践之基于OAuth的FACEBOOK Web Login（最新版）',
+          core_tech: 'JavaScript',
+          description: '通过采用 JavaScript 版 Facebook SDK 的“Facebook 登录”，用户可以使用 Facebook 登录信息登录您的网页。'
+      },
     {
       demo_link: 'https://codepen.io/haoyang/pen/jrvrQq',
       img_link: 'https://ooo.0o0.ooo/2016/11/24/5836d81f48cd2.png',
